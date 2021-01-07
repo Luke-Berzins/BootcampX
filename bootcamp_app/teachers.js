64fc330da4fdfa863bf2f3020959eeb977077cd5
luke-berzins <lukeberzins16@gmail.com>
2021-01-07 14:25:59 +0000

    adding sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,8 @@
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
 const { Pool } = require('pg');
-let myArgs = process.argv.slice(2);
+
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,16 +11,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher,
 cohorts.name as cohort 
 FROM teachers
 JOIN assistance_requests on teachers.id = teacher_id
 INNER JOIN cohorts on teacher_id = teachers.id
-WHERE cohorts.name = '${myArgs[0]}';
-`)
+WHERE cohorts.name LIKE $1;
+`
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
   })
 });
+
+
